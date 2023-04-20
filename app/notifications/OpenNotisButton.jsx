7303267952b07a28e442c90e296c8e0c4baec867
AlejoFernandez10 <alejofernandez1239@gmail.com>
2023-04-20 16:31:16 -0300

    Agrego item de cantidad al boton de notificaciones

--- app/notifications/OpenNotisButton.jsx
+++ app/notifications/OpenNotisButton.jsx
@@ -20,9 +20,10 @@ const OpenNotisButton = () => {
 
   return (
     <div>
-      <button onClick={()=> openNotisMenu()} className='  rounded-md   text-[25px] font-medium p-2 sm:mt-0 hover:bg-gray-700 hover:bg-opacity-40 text-[#EC7C6A]'>
-
+      <button onClick={()=> openNotisMenu()} className='  rounded-md  relative text-[25px] font-medium p-2 sm:mt-0 hover:bg-gray-700 hover:bg-opacity-40 text-[#EC7C6A]'>
+ 
       <IoMdNotificationsOutline  />
+      <span className='absolute top-0 right-0 bg-[#EC7C6A] rounded-full px-[6px] text-xs  text-gray-50'> 0 </span>
       </button>
 
       <Notifications state={open}/>
